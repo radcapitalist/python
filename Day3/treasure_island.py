8e8b58c1fd02a308109fe84ba6f573caec7cff6d
Eric Hill <eric.hill@jmp.com>
2024-03-18 16:27:57 -0400

    treasure island

---
+++ Day3/treasure_island.py
@@ -0,0 +1,51 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+in_turn_dir = input('Do you want to turn left or right? ')
+turn_dir = in_turn_dir.lower()
+if turn_dir != 'left':
+  print('Fall into a hole.\nGame Over.')
+  raise SystemExit
+
+in_swim_or_wait = input('Do you want to swim or wait? ')
+swim_or_wait = in_swim_or_wait.lower()
+if swim_or_wait != 'wait':
+  print('Attacked by trout.\nGame Over.')
+  raise SystemExit
+
+in_door = input('Which door do you want to go through: red, yellow or blue? ')
+door = in_door.lower()
+if door == 'red':
+  print('Burned by fire.\nGame Over.')
+elif door == 'blue':
+  print('Eaten by beasts.\nGame Over.')
+elif door != 'yellow':
+  print('Game Over.')
+elif door == 'yellow':
+  print('You win!');
