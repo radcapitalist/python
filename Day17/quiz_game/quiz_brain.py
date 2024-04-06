d4a97437354f4c9dbe6a2f7125624a722ba22173
Eric Hill <eric.hill@outlook.com>
2024-04-06 12:58:38 -0400

    tweaks

--- Day17/quiz_game/quiz_brain.py
+++ Day17/quiz_game/quiz_brain.py
@@ -6,23 +6,26 @@ class QuizBrain:
         self._n_correct = 0
         self._n_incorrect = 0
 
+    def _get_curr_question(self):
+        return self._questions[self._question_number - 1]
+    
+    def _is_answer_correct(self, str_answer, correct_answer):
+        al = str_answer.lower()
+        b_answer = True
+        if al == "false" or al == "f":
+            b_answer = False
+        correct = b_answer == correct_answer
+        if correct:
+            print(f'{b_answer} is correct!')
+        else:
+            print(f'{b_answer} is incorrect!')
+        return correct
+
+
     def _next_question(self):
-        q = None
-        if self._question_number <= len(self._questions):
-            q = self._questions[self._question_number - 1]
-        if q:
-            str_answer = input(f'\nQ.{self._question_number}: {q.question} True or False? ').lower()
-            if str_answer == 'false':
-                answer = False
-            else:
-                answer = True
-            print(f'Your answer was: {answer}')
-            if answer == q.answer:
-                print("Correct!")
-                return True
-            else:
-                print("Incorrect!")
-                return False
+        q = self._get_curr_question()
+        str_answer = input(f'\nQ.{self._question_number}: {q.question} True or False? ')
+        return self._is_answer_correct(str_answer, q.answer)
 
     def give_quiz(self):
         while self._question_number < len(self._questions):
@@ -32,6 +35,7 @@ class QuizBrain:
                 self._n_correct += 1
             else:
                 self._n_incorrect += 1
+            print(f'Current score is: {self._n_correct}/{self._n_correct + self._n_incorrect}')
 
-        print(f'You got {self._n_correct} correct and {self._n_incorrect} wrong.')
+        print(f'\nYou got {self._n_correct} correct and {self._n_incorrect} wrong.')
 
