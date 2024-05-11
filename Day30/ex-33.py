a7f07396d8df2e5007279b06f061ed8933615847
Eric Hill <eric.hill@outlook.com>
2024-05-10 23:02:21 -0400

    prep for flight

---
+++ Day30/ex-33.py
@@ -0,0 +1,14 @@
+fruits = eval(input())
+# 🚨 Do not change the code above
+
+# TODO: Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    fruit = fruits[index]
+    print(fruit + " pie")
+
+
+# 🚨 Do not change the code below
+make_pie(4)
+
+#input:
+["Apple", "Pear", "Orange"]
---
+++ Day30/ex-34.py
@@ -0,0 +1,14 @@
+
+# eval() function will create a list of dictionaries using the input
+facebook_posts = eval(input())
+
+total_likes = 0
+# TODO: Catch the KeyError exception
+for post in facebook_posts:
+    total_likes = total_likes + post['Likes']
+
+
+print(total_likes)
+
+# input:
+[{'Likes': 21, 'Comments': 2}, {'Likes': 13, 'Comments': 2, 'Shares': 1}, {'Likes': 33, 'Comments': 8, 'Shares': 3}, {'Comments': 4, 'Shares': 2},{'Comments': 1, 'Shares': 1}, {'Likes': 19, 'Comments': 3}]
