0f706ac1b00c0d8aaf5f7b67835e526b2a1b6d8c
Eric Hill <eric.hill@outlook.com>
2024-07-16 20:30:00 -0400

    progress on spotify app

--- Day46/main.py
+++ Day46/main.py
@@ -3,6 +3,9 @@ from datetime import date
 from bs4 import BeautifulSoup
 import os
 import requests
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 H100_URL_BASE = 'https://www.billboard.com/charts/hot-100/'
 
@@ -19,7 +22,26 @@ print(URL)
 response = requests.get(url=URL)
 hot_100_page = response.text
 soup = BeautifulSoup(hot_100_page, 'html.parser');
+
 song_h3s = soup.select('div.o-chart-results-list-row-container li.o-chart-results-list__item h3#title-of-a-story')
 titles = [song.getText().strip() for song in song_h3s]
-print(titles)
-artist_spans = soup.select('div.o-chart-results-list-row-container li.o-chart-results-list__item span')
+print(f'{len(titles)}\n{titles}')
+
+load_dotenv()
+
+spotify_client_id = os.environ.get("SPOTIFY_CLIENT_ID")
+spotify_secret = os.environ.get("SPOTIFY_CLIENT_SECRET")
+spotify_redirect_uri = "http://example.com"
+
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id,
+                                               client_secret=spotify_secret,
+                                               redirect_uri=spotify_redirect_uri,
+                                               scope=scope))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " - ", track['name'])
+
